053b77a142d811c03f77c1ede52fbfc4521a94ec
Acee Zai Mendez <your_email@example.com>
2026-05-27 16:44:50 +0800

    Added setters

--- fan_class/main.py
+++ fan_class/main.py
@@ -11,14 +11,17 @@ class Fan:
         self.color = color
         self.on = on
 
-    def get_speed(self):
+    def get_speed(self, speed):
         return self.__speed
 
-    def get_radius(self):
-        return self.__radius
+    def get_radius(self, radius):
+        return self.__radius = radius
 
-    def get_color(self):
-        return self.__color
+    def get_color(self, color):
+        return self.__color = color
+
+    def get_on(self, on):
+        return self.__on = on
 
 fan1 = Fan(Fan.FAST, 10, "yellow", True)
 fan2 = Fan(Fan.MEDIUM, 5, "blue", False)
