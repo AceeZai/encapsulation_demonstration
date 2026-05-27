d5c4b19df16c523e8e6e7db0d497024352467e0c
Acee Zai Mendez <your_email@example.com>
2026-05-27 17:09:41 +0800

    Finalized output for Fan 2

--- fan_class/main.py
+++ fan_class/main.py
@@ -39,4 +39,4 @@ fan1 = Fan(Fan.FAST, 10, "yellow", True)
 fan2 = Fan(Fan.MEDIUM, 5, "blue", False)
 
 print("Fan 1 Speed:", fan1.get_speed(), fan1.get_radius(), fan1.get_color(), fan1.get_on())
-
+print("Fan 2 Speed:", fan2.get_speed(), fan2.get_radius(), fan2.get_color(), fan2.get_on())
