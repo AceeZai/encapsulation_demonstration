1f178ccf2a424d7845eac5c17eab7f7e9f765600
Acee Zai Mendez <your_email@example.com>
2026-05-27 16:16:49 +0800

    Print fan attributes

--- fan_class/main.py
+++ fan_class/main.py
@@ -8,12 +8,13 @@ class Fan:
     def __init__(self, speed=1, radius=5, color="blue", on=False):
         self.speed = speed
         self.radius = radius
-        self.color = blue
+        self.color = color
         self.on = on
 
-        fan1 = Fan(Fan.FAST, 10, "yellow", True)
-        fan2 = Fan(Fan.MEDIUM, 5, "blue", False)
+fan1 = Fan(Fan.FAST, 10, "yellow", True)
+fan2 = Fan(Fan.MEDIUM, 5, "blue", False)
 
+print(fan1.speed, fan1.radius, fan1.color, fan1.on)
 fan = Fan()
 
 print(fan.speed)
