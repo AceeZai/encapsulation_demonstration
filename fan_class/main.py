f77c1bed16360b0525f0a64f7b4075663fa4ba07
Acee Zai Mendez <your_email@example.com>
2026-05-27 16:33:26 +0800

    Test multiple fan objects

--- fan_class/main.py
+++ fan_class/main.py
@@ -17,10 +17,14 @@ class Fan:
     def get_radius(self):
         return self.__radius
 
+    def get_color(self):
+        return self.__color
+
 fan1 = Fan(Fan.FAST, 10, "yellow", True)
 fan2 = Fan(Fan.MEDIUM, 5, "blue", False)
 
 print("Fan 1 Speed:", fan1.get_speed())
+print("Fan 2 Speed:", fan2.get_speed())
 
 print(fan.color)
 print(fan.on)
