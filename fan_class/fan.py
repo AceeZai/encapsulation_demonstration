2b7b44c9f06c99e45429335b913b3191cac31dbe
Acee Zai Mendez <your_email@example.com>
2026-05-27 17:25:53 +0800

    Finalized fan.py and test-fan.py

---
+++ fan_class/fan.py
@@ -0,0 +1,22 @@
+class Fan:
+    SLOW = 1
+    MEDIUM = 2
+    FAST = 3
+
+    def __init__(self, speed=1, radius=5, color="blue", on=False):
+        self.__speed = speed
+        self.__radius = radius
+        self.__color = color
+        self.__on = on
+
+    def get_speed(self):
+        return self.__speed
+
+    def get_radius(self):
+        return self.__radius
+
+    def get_color(self):
+        return self.__color
+
+    def get_on(self):
+        return self.__on
--- fan_class/test_fan.py
+++ fan_class/test_fan.py
@@ -1,4 +1,7 @@
+from fan import Fan
+
 fan1 = Fan(Fan.FAST, 10, "yellow", True)
 fan2 = Fan(Fan.MEDIUM, 5, "blue", False)
 
-print(...)
+print("Fan 1:", fan1.get_speed(), fan1.get_radius(), fan1.get_color(), fan1.get_on())
+print("Fan 2:", fan2.get_speed(), fan2.get_radius(), fan2.get_color(), fan2.get_on())
