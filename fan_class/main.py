256ce51e6c6f1666b5356d42e4dc2caaa63ce61b
Acee Zai Mendez <your_email@example.com>
2026-05-27 16:24:10 +0800

    Fixing speed

--- fan_class/main.py
+++ fan_class/main.py
@@ -11,13 +11,13 @@ class Fan:
         self.color = color
         self.on = on
 
+    def get_speed(self):
+        return self.__speed
+
 fan1 = Fan(Fan.FAST, 10, "yellow", True)
 fan2 = Fan(Fan.MEDIUM, 5, "blue", False)
 
 print(fan1.speed, fan1.radius, fan1.color, fan1.on)
-
-fan = Fan()
-print(fan.__speed)
 
 print(fan.radius)
 print(fan.color)
