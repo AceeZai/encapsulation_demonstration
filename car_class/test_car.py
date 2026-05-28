8384941a8f74c85c061a4eae2b2ce7947be1abae
Acee Zai Mendez <your_email@example.com>
2026-05-28 15:02:45 +0800

    Finalize speed

--- car_class/test_car.py
+++ car_class/test_car.py
@@ -1,13 +1,16 @@
+import time
+from car import Car
+
 print("Starting Car Speed Simulation")
 
-from car import Car
 my_car = Car(2020, "Toyota")
 
 print("\n--- Accelerating ---")
 
 for count in range(5):
     my_car.accelerate()
-print(my_car.get_speed())
+    speed = my_car.get_speed()
+    print("The car is now running at", speed, "mph.", "|" * speed)
 
 for count in range(5):
     my_car.brake()
@@ -16,4 +19,4 @@ print(my_car.get_speed())
 speed = my_car.get_speed()
 
 print("Speed:", speed, "|" * speed)
-print("|" * 2.5)
+print("|" * speed)
