f21d88746c0d24fb61e09ddaa0dab30c1aa36217
Acee Zai Mendez <your_email@example.com>
2026-05-28 15:03:54 +0800

    Finalize brake

--- car_class/test_car.py
+++ car_class/test_car.py
@@ -11,10 +11,11 @@ for count in range(5):
     my_car.accelerate()
     speed = my_car.get_speed()
     print("The car is now running at", speed, "mph.", "|" * speed)
+    time.sleep(0.5)
 
 for count in range(5):
     my_car.brake()
-print(my_car.get_speed())
+    speed = my_car.get_speed()
 
 speed = my_car.get_speed()
 
