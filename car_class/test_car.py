86c5d3534d5ef0d837111b5f7936ee438428c2de
Acee Zai Mendez <your_email@example.com>
2026-05-28 14:41:58 +0800

    Add speed visualization

--- car_class/test_car.py
+++ car_class/test_car.py
@@ -12,3 +12,8 @@ print(my_car.get_speed())
 for count in range(5):
     my_car.brake()
 print(my_car.get_speed())
+
+speed = my_car.get_speed()
+
+print("Speed:", speed, "|" * speed)
+print("|" * 2.5)
