8787d60d1dc82ba5b4a7eec7611959e3b73d4776
Acee Zai Mendez <your_email@example.com>
2026-05-29 23:26:42 +0800

    Display pet information

--- pet_class/test_pet.py
+++ pet_class/test_pet.py
@@ -8,3 +8,7 @@ age = int(input("Enter your pet's age: "))
 my_pet.set_name(name)
 my_pet.set_animal_type(animal_type)
 my_pet.set_age(age)
+
+print("Pet Name:", my_pet.get_name())
+print("Animal Type:", my_pet.get_animal_type())
+print("Pet Age:", my_pet.get_age())
