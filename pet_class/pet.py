7231e518f5309435214a04e81b788f16f8413531
Acee Zai Mendez <your_email@example.com>
2026-05-29 23:39:39 +0800

    Debugged errors

--- pet_class/pet.py
+++ pet_class/pet.py
@@ -5,14 +5,14 @@ class Pet:
         self.__animal_type = animal_type
         self.__age = age
 
-    def set_name(self):
-        return self.__name
+    def set_name(self, name):
+        self.__name = name
 
     def set_animal_type(self, animal_type):
-        return self.__animal_type
+        self.__animal_type = animal_type
 
     def set_age(self, age):
-        return self.__age
+        self.__age = age
 
     def get_name(self):
         return self.__name
