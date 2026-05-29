91cb2e83612824a2c522cbd24cb6a4bbdfd8d7d3
Acee Zai Mendez <your_email@example.com>
2026-05-29 23:28:59 +0800

    Add section separator formatting and pet sound feature

--- pet_class/test_pet.py
+++ pet_class/test_pet.py
@@ -9,6 +9,11 @@ my_pet.set_name(name)
 my_pet.set_animal_type(animal_type)
 my_pet.set_age(age)
 
+print("=" * 40)
+
 print("Pet Name:", my_pet.get_name())
 print("Animal Type:", my_pet.get_animal_type())
 print("Pet Age:", my_pet.get_age())
+
+print("\nPet Sound:")
+my_pet.pet_sound()
