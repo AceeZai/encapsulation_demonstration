e7be34599e7dacef041aabd0882f8f9b968e5120
Acee Zai Mendez <your_email@example.com>
2026-05-30 12:00:20 +0800

    Added feature for fan2

--- fan_class/test_fan.py
+++ fan_class/test_fan.py
@@ -7,3 +7,4 @@ print("Fan 1:", fan1.get_speed(), fan1.get_radius(), fan1.get_color(), fan1.get_
 print("Fan 2:", fan2.get_speed(), fan2.get_radius(), fan2.get_color(), fan2.get_on())
 
 fan1.display_info()
+fan2.display_info()
